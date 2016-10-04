827cda0f0a810fdb1af50d56137e6a423607f783
stavroskasidis <stavroskasidis@outlook.com>
2016-10-04 19:12:37 +0300

    iOS Synchronization implemented (need testing,,)

--- Erebus/Mobile/Erebus.Mobile.iOS/PlatformImplementations/iOSSynchronizationServiceManager.cs
+++ Erebus/Mobile/Erebus.Mobile.iOS/PlatformImplementations/iOSSynchronizationServiceManager.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using UIKit;
 
 namespace Erebus.Mobile.iOS.PlatformImplementations
 {
@@ -11,12 +11,13 @@ namespace Erebus.Mobile.iOS.PlatformImplementations
     {
         public void StartSynchronizationService()
         {
-            throw new NotImplementedException();
+            UIApplication.SharedApplication.SetMinimumBackgroundFetchInterval(UIApplication.BackgroundFetchIntervalMinimum);
         }
 
         public void StopSynchronizationService()
         {
-            throw new NotImplementedException();
+
+            UIApplication.SharedApplication.SetMinimumBackgroundFetchInterval(UIApplication.BackgroundFetchIntervalNever);
         }
     }
 }
