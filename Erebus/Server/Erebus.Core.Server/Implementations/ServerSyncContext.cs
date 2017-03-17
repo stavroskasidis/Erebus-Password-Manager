9661facedf3ad3f3b6aadca080110a61b002887f
stavroskasidis <stavroskasidis@outlook.com>
2017-03-17 22:51:26 +0200

    Changed sync context to work on linux

--- Erebus/Server/Erebus.Core.Server/Implementations/ServerSyncContext.cs
+++ Erebus/Server/Erebus.Core.Server/Implementations/ServerSyncContext.cs
@@ -4,21 +4,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Erebus.Core.Server.Implementations
 {
     public class ServerSyncContext : ISyncContext
     {
-        private EventWaitHandle waitHandle = new EventWaitHandle(true, EventResetMode.AutoReset, "erebus_sync_context_470B3552-D81A-4913-A928-74201783FE14");
+
+        //Named EventWaitHandles are not supported on linux :(
+
+        /*====================================*/
+        //private EventWaitHandle waitHandle = new EventWaitHandle(true, EventResetMode.AutoReset, "erebus_sync_context_470B3552-D81A-4913-A928-74201783FE14");
+
+        //public void Lock()
+        //{
+        //    waitHandle.WaitOne();
+        //}
+
+        //public void Release()
+        //{
+        //    waitHandle.Set();
+        //}
+
+        /*====================================*/
+
+
+        private static volatile bool IsLocked = false;
 
         public void Lock()
         {
-            waitHandle.WaitOne();
+            while (IsLocked)
+            {
+                Task.Delay(10).Wait();
+            }
+
+            IsLocked = true;
         }
 
         public void Release()
         {
-            waitHandle.Set();
+            IsLocked = false;
         }
     }
 }
